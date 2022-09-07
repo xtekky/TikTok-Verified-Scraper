955f1febf07f56d195b47a78ead155a4ccc831bc
xtekky <fkahdias@gmail.com>
2022-09-07 22:55:56 +0200

    unpatch that shit

---
+++ main.py
@@ -0,0 +1,87 @@
+import time
+import string
+import requests
+import itertools
+
+from urllib.parse import urlencode
+from utils.signer import *
+
+class Verifinder:
+    def __init__(self, proxy: str or None = None, count: int = 4) -> None:
+        self.proxies  = {'http': f'http://{proxy}', 'https': f'http://{proxy}'} if proxy else None
+        self.accounts = []
+        self.keywords = get_keywords(count)
+
+    def __base_params(self, keyword: str, cursor: int = 0) -> str:
+        return urlencode({
+            "count"             : 30,
+            "cursor"            : cursor,
+            "keyword"           : keyword,
+            "search_source"     : "report_user",
+            "type"              : 1,
+            "request_tag_from"  : "h5",
+            "storage_type"      : 0,
+            "iid"               : 7137816409338136325,
+            "channel"           : "googleplay",
+            "device_type"       : "SM-G973N",
+            "device_id"         : 6990239216324986369,
+            "os_version"        : 9,
+            "version_code"      : 160904,
+            "app_name"          : "musically_go",
+            "device_brand"      : "samsung",
+            "device_platform"   : "android",
+            "aid"               : 1340,
+        })
+        
+    def __base_headers(self, params: str) -> dict:
+        sig = XGorgon(
+            params = params
+        ).get_value()
+        
+        return {
+            "accept-encoding"   : "gzip",
+            "sdk-version"       : "2",
+            "x-ss-req-ticket"   : str(int(time.time() * 1000)),
+            "x-khronos"         : sig["X-Khronos"],
+            "x-gorgon"          : sig["X-Gorgon"],
+            "host"              : "api16-normal-c-useast1a.tiktokv.com",
+            "connection"        : "Keep-Alive",
+            "user-agent"        : "okhttp/3.10.0.1"
+        }
+    
+    def __scrape_veris(self, keyword: str, cursor: int = 0) -> requests.Response:
+        __base_params = self.__base_params(keyword, cursor)
+
+        return requests.get(
+            url = (
+                "https://api16-normal-c-useast1a.tiktokv.com"
+                    + "/aweme/v1/discover/search/?"
+                    + __base_params 
+            ),
+            headers = self.__base_headers(__base_params)
+        )
+        
+    def main(self):
+        cursor  = 0
+        for keyword in self.keywords:
+            while True:
+                try:
+                    __scrape_req = self.__scrape_veris(keyword, cursor)
+                    # print(__scrape_req.text)
+                    for _ in __scrape_req.json()["user_list"]:
+                        if _["user_info"]["unique_id"] not in self.accounts:
+                            self.accounts.append(_["user_info"]["unique_id"])
+                            print(f'{_["user_info"]["unique_id"]}:{_["user_info"]["follower_count"]}:{_["user_info"]["uid"]}:{_["user_info"]["sec_uid"]}:{_["user_info"]["region"]}')
+                    
+                    if len(__scrape_req.json()["user_list"]) == 0:
+                        cursor = 0
+                        break
+                    
+                    cursor += 30 if cursor < 30 else 31
+                
+                except Exception:
+                    cursor = 0
+                    break
+                
+if __name__ == "__main__":
+    Verifinder().main()
---
+++ utils/signer.py
@@ -0,0 +1,149 @@
+import json
+import time
+import string
+import hashlib
+import itertools
+
+class XGorgon:
+    def __init__(self, params: str, data: str = None, cookies: str = None) -> None:
+
+        self.params = params
+        self.data = data
+        self.cookies = cookies
+
+    def __hash(self, data: str) -> str:
+        _hash = str(
+            hashlib.md5(
+                data.encode()
+            ).hexdigest()
+        )
+
+        return _hash
+
+    def __base_string(self) -> str:
+        base_str = self.__hash(self.params)
+        base_str = (
+            base_str + self.__hash(self.data) 
+            if self.data
+            else base_str + str("0" * 32)
+        )
+        base_str = (
+            base_str + self.__hash(self.cookies)
+            if self.cookies
+            else base_str + str("0" * 32)
+        )
+
+        return base_str
+
+    def get_value(self) -> json:
+        base_str = self.__base_string()
+
+        return self.__encrypt(base_str)
+
+    def __encrypt(self, data: str) -> json:
+        unix = int(time.time())
+        len = 0x14
+        key = [
+            0xDF,
+            0x77,
+            0xB9,
+            0x40,
+            0xB9,
+            0x9B,
+            0x84,
+            0x83,
+            0xD1,
+            0xB9,
+            0xCB,
+            0xD1,
+            0xF7,
+            0xC2,
+            0xB9,
+            0x85,
+            0xC3,
+            0xD0,
+            0xFB,
+            0xC3,
+        ]
+
+        param_list = []
+
+        for i in range(0, 12, 4):
+            temp = data[8 * i : 8 * (i + 1)]
+            for j in range(4):
+                H = int(temp[j * 2 : (j + 1) * 2], 16)
+                param_list.append(H)
+
+        param_list.extend([0x0, 0x6, 0xB, 0x1C])
+
+        H = int(hex(unix), 16)
+
+        param_list.append((H & 0xFF000000) >> 24)
+        param_list.append((H & 0x00FF0000) >> 16)
+        param_list.append((H & 0x0000FF00) >> 8)
+        param_list.append((H & 0x000000FF) >> 0)
+
+        eor_result_list = []
+
+        for A, B in zip(param_list, key):
+            eor_result_list.append(A ^ B)
+
+        for i in range(len):
+
+            C = self.__reverse(eor_result_list[i])
+            D = eor_result_list[(i + 1) % len]
+            E = C ^ D
+
+            F = self.__rbit_algorithm(E)
+            H = ((F ^ 0xFFFFFFFF) ^ len) & 0xFF
+            eor_result_list[i] = H
+
+        result = ""
+        for param in eor_result_list:
+            result += self.__hex_str(param)
+
+        return {
+            "X-Gorgon": (
+                "0404b0d30000" + result
+            ), 
+            "X-Khronos": str(unix)
+        }
+
+    def __rbit_algorithm(self, num):
+        result = ""
+        tmp_string = bin(num)[2:]
+
+        while len(tmp_string) < 8:
+            tmp_string = "0" + tmp_string
+
+        for i in range(0, 8):
+            result = result + tmp_string[7 - i]
+
+        return int(result, 2)
+
+    def __hex_str(self, num):
+        tmp_string = hex(num)[2:]
+
+        if len(tmp_string) < 2:
+            tmp_string = "0" + tmp_string
+
+        return tmp_string
+
+    def __reverse(self, num):
+        tmp_string = self.__hex_str(num)
+
+        return int(tmp_string[1:] + tmp_string[:1], 16)
+    
+def iter_all_strings():
+    for size in itertools.count(1):
+        for s in itertools.product(string.ascii_lowercase, repeat=size):
+            yield "".join(s)
+
+def get_keywords(count: int):
+    letter_list = []
+    for s in iter_all_strings():
+        # print(s)
+        letter_list.append(s)
+        if s == 'z'*count:
+            break
+    return letter_list
